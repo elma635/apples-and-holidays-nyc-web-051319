c6ca7e8e52b5a21fe3a78f5ca01383ff9fb0e625
Elma Rahman <elmarahman635@gmail.com>
2019-04-21 21:55:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,8 +72,8 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
-  #holiday_hash.collect do |holiday , supplies|
-    #holiday , supplies
+  holiday_hash.collect do |holiday , supplies|
+    [:holiday] 
 
   end
 
