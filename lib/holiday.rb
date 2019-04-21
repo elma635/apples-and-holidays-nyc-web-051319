5d6370cc50c139c3465eb2ddeb189f27eb3459e6
Elma Rahman <elmarahman635@gmail.com>
2019-04-21 22:48:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |seasons, holidays| #holidays contains all holidays & values for it
     seasons.to_s.capitalize! << ":"  #push a string onto a string , colon by itself is protected keyword.
    holidays.map do |holiday, supplies|
-binding.pry
+holiday.to_s.split("_").map {|word| word.capitalize}.join(" ")
     #  season.to_s.capitalize!
     #  supplies.split(",").capitalize!
 
